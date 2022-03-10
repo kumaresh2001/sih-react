5692d12c6d43be573247bf1af169687946e2dd27
Suraj SK <surajsk2002@gmail.com>
2022-03-10 09:26:32 +0530

    new results

---
+++ src/assets/Results2.js
@@ -0,0 +1,36 @@
+
+export const Results1 = {
+    "writer":[{"name":"University of Oxford(United Kingdom)", "img":require("../assets/oxford.jpg"), 
+    "requirements":["i)IELTS -7.5(Minimum 7.0 per component )" , "ii)TOEFL/ TOEFL iBT Home Edition - 110" , "iii) Cambridge Certificate of Advanced English (CAE) -191(Minimum 185 per component)"]},
+    {"name":"University of Cambridge(United Kingdom)", "img":require("../assets/cambridge.jpg"), 
+    "requirements":["i)A Level English Literature","ii)International Baccalaureate Diploma Programme-40-42","iii)UCAS Tariff- 152."]},
+    {"name":"St. Stephen's College(New Delhi,India )", "img":require("../assets/Stephen's.jpg"), 
+    "requirements":"English Core 90% Or Elective English 85%"},
+    {"name":"Lady Shri Ram College for Women(New Delhi,India )", "img":require("../assets/LadiShri.jpg"), 
+    "requirements":" Required cut off -96.5"},
+    {"name":"Rabindra Bharati University (Kolkata,west Bengal)", "img":require("../assets/rabindra.jpg"), 
+    "requirements":" Required cut off - 89-95"},
+    {"name":"St. Xavier's College, (Mumbai,India)", "img":require("../assets/xavier.jpg"), 
+    "requirements":"Required cut off - 76-94"},
+    {"name":"Madras Christian College( Chennai, Tamil Nadu, India )", "img":require("../assets/christiancollege.jpg"), 
+    "requirements":"Required cut off - 94"}
+]}
+
+export const Results2 = {
+    "entrepreneur":[{"name":"Harvard University(US)", "img":require("../assets/harvard.jpg"), 
+    "requirements":["i) You should have minimum of 75th percentile, with a 1580 SAT or a 35 ACT." , "ii)Required GPA - 4.18  or higher. " , "iii)If your GPA is lower than this, you need to compensate with a higher SAT/ACT score."]},
+    {"name":"University of Pennsylvania", "img":require("../assets/pennsylvania.jpg"), 
+    "requirements":["i) You should have minimum of 75th percentile, with a 1560 SAT or a 35 ACT." , "ii)Required GPA - 3.9  or higher. " , "iii)If your GPA is lower than this, you need to compensate with a higher SAT/ACT score."]},
+    {"name":"Indian Institute of Management (IIM), Indore", "img":require("../assets/IIM.jpg"), 
+    "requirements":"Entrance Exam(IPM Aptitude Test)"},
+    {"name":"Shaheed Sukhdev College of Business Studies, Delhi University", "img":require("../assets/shaheed.png"), 
+    "requirements":" CUCET(Central University Common Entrance Test)"},
+    {"name":"Keshav Mahavidyalay, Delhi University", "img":require("../assets/keshav.jpg"), 
+    "requirements":"CUCET(Central University Common Entrance Test)"},
+    {"name":"Indian Institute of Management (IIM), Rohtak", "img":require("../assets/IIM2.jpg"), 
+    "requirements":"Entrance Exam(IPM Aptitude Test)"},
+    {"name":"DeenDayal Upadhyay College, Delhi University", "img":require("../assets/Deen-Dayal.jpg"), 
+    "requirements":"CUCET(Central University Common Entrance Test)"},
+    {"name":"Symbiosis Centre for Management Studies, Pune", "img":require("../assets/Symbiosis.jpg"), 
+    "requirements":"SET (Symbiosis Entrance Test)"}
+]}

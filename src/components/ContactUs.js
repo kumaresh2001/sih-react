7cb9a5dc9871cc5f0b6e9696ea76f46f6233c48f
Suraj SK <surajsk2002@gmail.com>
2022-07-31 11:05:20 +0530

    contacts update

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import emailjs from "emailjs-com";
 import "../styles/contact.css";
+import pic from "../assets/contactus.jpg"
 export default function ContactUs() {
 
     function sendEmail(e) {
@@ -19,27 +20,36 @@ export default function ContactUs() {
     return(
         <div>
             <div className="container">
-                <h1>GET IN TOUCH WITH US</h1>
-                <p>Want to get in touch? We would love to hear from you. Here's how you can reach us...</p>
-            <form onSubmit={sendEmail}>
-                    <div className="mainForm">
-                        <div className="formItems">
-                            <input type="text" className="name" placeholder="Name" name="name" required/>
-                        </div>
-                        <div className="formItems">
-                            <input type="email" className="email" placeholder="Email Address" name="email" required/>
-                        </div>
-                        <div className="formItems">
-                            <input type="text" className="subject" placeholder="Subject" name="subject" required/>
-                        </div>
-                        <div className="formItems">
-                            <textarea className="message" cols="30" rows="8" placeholder="Your message" name="message" required></textarea>
-                        </div>
-                        <div className="formSubmit">
-                            <input type="submit" className="submit" value="Send Message"></input>
-                        </div>
+                <h1>GET IN TOUCH</h1>
+                <p><i>Do you need help? That's what the Meraki team is for!</i></p>
+                <p>Please submit your queries here, and we will get back to you as soon as possible.</p>
+                <div class="row">
+                    <div class="column">
+                        <img src={pic} className = "contactImage"/>
                     </div>
-                </form>
+                    <div class="column">
+                        <form onSubmit={sendEmail}>
+                            <div className="mainForm">
+                                <div className="formItems">
+                                    <input type="text" className="name" placeholder="Name" name="name" required/>
+                                </div>
+                                <div className="formItems">
+                                    <input type="email" className="email" placeholder="Email Address" name="email" required/>
+                                </div>
+                                <div className="formItems">
+                                    <input type="text" className="subject" placeholder="Subject" name="subject" required/>
+                                </div>
+                                <div className="formItems">
+                                    <textarea className="message" cols="30" rows="8" placeholder="Your message" name="message" required></textarea>
+                                </div>
+                                <div className="formSubmit">
+                                    <input type="submit" className="submit" value="Send Message"></input>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            
             </div>
         </div>
     )
